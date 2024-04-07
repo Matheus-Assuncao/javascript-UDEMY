765241501471d475338268552597f37fa1b48187
Matheus Assunção <130656978+Matheus-Assuncao@users.noreply.github.com>
2024-04-07 19:41:01 -0300

    Atualização do repositório
    Devido a mudança do local do diretório, o repositório acabou ficando desatualizado. Estou atualizando isso.

---
+++ aula120-validaCPFNovo/src/main.js
@@ -0,0 +1,9 @@
+import geraCPF from './modules/geraCPF'
+
+import './assets/css/style.css';
+
+(function(){
+    const gera = new geraCPF();
+    const cpfGerado = document.querySelector('.cpfGerado');
+    cpfGerado.innerHTML = gera.geraNovoCPF();
+})();
